ec158edb5cb55261e703248f5c36a6de391a2d1a
BharathKumarAI <bharath.aiexp@gmail.com>
2024-01-08 20:28:33 +0530

    Test code for gemini vision video code

---
+++ SmartEye/experiments/videogemini.py
@@ -0,0 +1,47 @@
+import http.client
+import typing
+import urllib.request
+
+import IPython.display
+from PIL import Image as PIL_Image
+from PIL import ImageOps as PIL_ImageOps
+from vertexai.preview.generative_models import (
+    GenerationConfig,
+    GenerativeModel,
+    Image,
+    Part,
+)
+
+if "google.colab" in sys.modules:
+    # Define project information
+    PROJECT_ID = "[your-project-id]"  # @param {type:"string"}
+    LOCATION = "us-central1"  # @param {type:"string"}
+
+    # Initialize Vertex AI
+    import vertexai
+
+    vertexai.init(project=PROJECT_ID, location=LOCATION)
+    
+multimodal_model = GenerativeModel("gemini-pro-vision")
+
+file_path = "github-repo/img/gemini/multimodality_usecases_overview/pixel8.mp4"
+video_uri = f"gs://{file_path}"
+video_url = f"https://storage.googleapis.com/{file_path}"
+
+IPython.display.Video(video_url, width=450)
+
+prompt = """
+Answer the following questions using the video only:
+What is the profession of the main person?
+What are the main features of the phone highlighted?
+Which city was this recorded in?
+Provide the answer JSON.
+"""
+
+video = Part.from_uri(video_uri, mime_type="video/mp4")
+contents = [prompt, video]
+
+responses = multimodal_model.generate_content(contents, stream=True)
+
+for response in responses:
+    print(response.text, end="")
